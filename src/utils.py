8178e6a0e489f6b9b77da3c243f0b53815562d99
Mohammad Farooque Ahmed <2306033@kiit.ac.in>
2026-05-15 00:37:25 +0530

    Model Training Done

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,21 @@
 import os
 import sys
-
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
 
 def save_object(file_path, obj):
+    """
+    Saves a python object to a specific path using dill/pickle.
+    """
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
@@ -20,75 +23,53 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
-def evaluate_models(xtrain,ytrain,xtest,ytest,models):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """
+    Automatically trains multiple models, performs Hyperparameter tuning, 
+    and returns a report of R2 scores.
+    """
     try:
         report = {}
-        for i in range(len(models)):
+
+        for i in range(len(list(models))):
             model = list(models.values())[i]
-            # Train model
-            model.fit(xtrain,ytrain)
+            para = param[list(models.keys())[i]]
 
-            # Predict Training data
-            y_train_pred = model.predict(xtrain)
+            # Automatic Hyperparameter Tuning (Grid Search)
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
-            # Predict Testing data
-            y_test_pred =model.predict(xtest)
+            # Set the model to use the best parameters found
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
-            # Get R2 scores for train and test data
-            train_model_score = r2_score(ytrain,y_train_pred)
-            test_model_score = r2_score(ytest,y_test_pred)
+            # Make Predictions
+            y_test_pred = model.predict(X_test)
 
-            report[list(models.keys())[i]] =  test_model_score
+            # Get R2 Score for the test data
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
     except Exception as e:
-        logging.info('Exception occured during model training')
-        raise CustomException(e,sys)
-    
+        raise CustomException(e, sys)
+
 def model_metrics(true, predicted):
-    try :
+    try:
         mae = mean_absolute_error(true, predicted)
         mse = mean_squared_error(true, predicted)
         rmse = np.sqrt(mse)
         r2_square = r2_score(true, predicted)
         return mae, rmse, r2_square
     except Exception as e:
-        logging.info('Exception Occured while evaluating metric')
-        raise CustomException(e,sys)
-    
+        raise CustomException(e, sys)
 
-def print_evaluated_results(xtrain,ytrain,xtest,ytest,model):
-    try:
-        ytrain_pred = model.predict(xtrain)
-        ytest_pred = model.predict(xtest)
-
-        # Evaluate Train and Test dataset
-        model_train_mae , model_train_rmse, model_train_r2 = model_metrics(ytrain, ytrain_pred)
-        model_test_mae , model_test_rmse, model_test_r2 = model_metrics(ytest, ytest_pred)
-
-        # Printing results
-        print('Model performance for Training set')
-        print("- Root Mean Squared Error: {:.4f}".format(model_train_rmse))
-        print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
-        print("- R2 Score: {:.4f}".format(model_train_r2))
-
-        print('----------------------------------')
-    
-        print('Model performance for Test set')
-        print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
-        print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
-        print("- R2 Score: {:.4f}".format(model_test_r2))
-    
-    except Exception as e:
-        logging.info('Exception occured during printing of evaluated results')
-        raise CustomException(e,sys)
-    
 def load_object(file_path):
     try:
-        with open(file_path,'rb') as file_obj:
+        with open(file_path, "rb") as file_obj:
             return dill.load(file_obj)
     except Exception as e:
-        logging.info('Exception Occured in load_object function utils')
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
